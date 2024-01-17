28b75e61aafaba038e93888b005f59e42f40d200
Dhruvil <thakordrvl@gmail.com>
2024-01-17 13:36:17 +0530

    implemented copying directory and hashing function

--- hashing.py
+++ hashing.py
@@ -2,15 +2,12 @@ import os
 import shutil
 import hashlib
 
-# print(hashlib.algorithms_guaranteed)
-current_directory_from_user = input("Enter your path: ")
+# current_directory_from_user = input("Enter your path: ")
 
-if(os.path.exists(current_directory_from_user)==False):
-    print("Invalid path kindly recompile")
-    exit()
+# if(os.path.exists(current_directory_from_user)==False):
+#     print("Invalid path kindly recompile")
+#     exit()
 
-files_and_directories = os.listdir(current_directory_from_user)
-files_in_json = []
 
 def compute_md5(file_path):
     initial_chunk_size = 4096
@@ -38,11 +35,42 @@ def get_file_info(file_path):
 
 def Generating_hash_files(input_directory):
 
+    files_and_directories = os.listdir(input_directory)
+    files_in_json = []
+    
     for item in files_and_directories:
         item_path = os.path.join(input_directory, item)
+        # print(item)
         
         if os.path.isfile(item_path):
             files_in_json.append(get_file_info(item_path))
+            
+    return files_in_json        
+
+def copy_directory(source_path, destination_path):
+    
+    source_directory_name = os.path.basename(os.path.normpath(source_path))
+    full_destination_path = os.path.join(destination_path, source_directory_name)
+    
+    print("reached here")
+    
+    if os.path.exists(full_destination_path):
                 
-Generating_hash_files(current_directory_from_user)
-print(files_in_json)
+        ans = input("There exist a directory with the same name please enter YES if you want to replace the existing directory ")
+        
+        if ans!="YES" : 
+            print("exiting program please recomplile")
+            exit()
+            
+        shutil.rmtree(full_destination_path)
+        
+    shutil.copytree(source_path,full_destination_path)
+    
+    return Generating_hash_files(full_destination_path)
+        
+        
+# files_in_json = Generating_hash_files(current_directory_from_user)
+source_path = input("please enter the path of the directory you want to copy : ")
+destination_path = input("please enter the path of the directory where you want to place the copied directory : ")
+files_in_json_after_copying = copy_directory(source_path,destination_path)
+print(files_in_json_after_copying)
