56e2a3aa231d93ce81494f325ba16b2a17caed75
Dhruvil <thakordrvl@gmail.com>
2024-01-22 15:45:48 +0530

    Implemented Help function

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ import sys
 import datetime
 import platform
 
+def print_usage_help():
+    print("drvl - A Version Control System.")
+    print("drvl init - Initialize a new drvl repository")
+    print("drvl add <file> - Add a file to the index")
+    print("drvl commit -m <message> - Commit changes with a message")
+    print("drvl rmadd <file> - Remove a file from the index")
+    print("drvl rmcommit - Remove last commit")
+    print("drvl log - Display commit log")
+    print("drvl checkout <commit> - Checkout a specific commit")
+    print("drvl help - To see this usage help")
+    print("drvl status - To see status")
+    print("drvl user show - To see present user")
+    print("drvl user set <username> - To change user")
+    print("drvl push <path> - To push your file to another folder")
+    print("Created by - Dhruvil")
+
 def drvl_makedirs(dir_path, user_name):
     drvl_path = os.path.join(dir_path, ".drvl")  # Add a space before ".drvl"
     os.makedirs(drvl_path)
@@ -51,9 +67,8 @@ class init:
             self.user = input("Provide a username: ")
             drvl_makedirs(self.curr_dir_path,self.user)
 
-if len(sys.argv) > 2 or len(sys.argv) == 1:
-    print("Invalid CLA, Exiting program, Kindly recompile")
-    exit()
+if len(sys.argv) == 1:
+    print_usage_help()
     
 elif sys.argv[1] == "init":
     dir_path = os.getcwd()
