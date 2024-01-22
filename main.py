a2f78093b4f0f7c32306a11b7bcd31a5f36e0e4f
Dhruvil <thakordrvl@gmail.com>
2024-01-22 17:23:15 +0530

    created several functions for status command

--- main.py
+++ main.py
@@ -6,6 +6,78 @@ import sys
 import datetime
 import platform
 
+def write_to_json(json_array, json_file_path):
+    with open(json_file_path, 'w') as json_file:
+        json.dump(json_array, json_file, indent=2)
+
+def compute_md5(file_path):
+    initial_chunk_size = 4096
+    max_chunk_size = 65536
+    hash_md5 = hashlib.md5()
+    chunk_size = initial_chunk_size
+    with open(file_path, "rb") as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            hash_md5.update(chunk)
+            if chunk_size < max_chunk_size:
+                chunk_size *= 2
+    return hash_md5.hexdigest()
+
+def get_file_info(file_path):
+    file_size = os.path.getsize(file_path)
+    md5_hash = compute_md5(file_path)
+    return {"Filename": os.path.basename(file_path), "file size": file_size, "md5 hash": md5_hash}
+
+def Generating_hash_file(input_directory):
+    files_and_directories = os.listdir(input_directory)
+    files_in_json = []
+    for item in files_and_directories:
+        item_path = os.path.join(input_directory, item)
+        if os.path.isfile(item_path):
+            files_in_json.append(get_file_info(item_path))
+
+    json_file_path = os.path.join(input_directory, "file_info.json")
+    write_to_json(files_in_json, json_file_path)
+    print("JSON file successfully created/updated at " + input_directory)
+    
+def add_file(file_name):
+    print()
+        
+def get_tracked_hashes(dir_path): 
+    json_path = os.path.join(dir_path,".drvl/branches/main/index.json")
+    
+    try:
+        with open(json_path, "r") as index_file:
+            index_data = json.load(index_file)
+            tracked_hashes = [file_info["md5 hash"] for file_info in index_data.values()]
+            return tracked_hashes
+    except FileNotFoundError:
+        return []
+
+def get_untracked_files(dir_path):
+    tracked_hashes = get_tracked_hashes(dir_path)
+    all_files = [file for file in os.listdir(dir_path) if os.path.isfile(file)]
+    untracked_files = []
+
+    for file in all_files:
+        file_hash = compute_md5(file)
+        if file_hash not in tracked_hashes:
+            untracked_files.append(file)
+
+    return untracked_files
+
+def print_status(dir_path):
+    untracked_files = get_untracked_files(dir_path)
+
+    if untracked_files:
+        print("Untracked files:")
+        for file in untracked_files:
+            print(f"- {file}")
+    else:
+        print("No untracked files.")
+
 def print_usage_help():
     print("drvl - A Version Control System.")
     print("drvl init - Initialize a new drvl repository")
@@ -47,11 +119,11 @@ def drvl_makedirs(dir_path, user_name):
         file.write("User: " + user_name)
         file.write("\n\n")
         
-def isexist(path):
+def isexist(file_name, path):
     files_directories = os.listdir(path)
     
     for item in files_directories:
-        if item == ".drvl":
+        if item == file_name:
             return True
         
     return False
@@ -63,17 +135,33 @@ class init:
     def __init__(self, dir_path):
         self.curr_dir_path = dir_path  # Use self.curr_dir_path to refer to the class attribute
         
-        if not isexist(self.curr_dir_path): 
+        if not isexist(".drvl",self.curr_dir_path): 
             self.user = input("Provide a username: ")
             drvl_makedirs(self.curr_dir_path,self.user)
+          
+dir_path = os.getcwd()
 
 if len(sys.argv) == 1:
     print_usage_help()
     
 elif sys.argv[1] == "init":
-    dir_path = os.getcwd()
     obj = init(dir_path)
-    print(dir_path)
+    # print(dir_path)
+    
+elif sys.argv[1]=="add":
+    if(isexist(dir_path,sys.argv[2])):
+        add_file(sys.argv[2])   
+        
+    else:
+        print("File doesnt exist !!")
+        exit()
+        
+elif sys.argv[1]=="status":
+    if(isexist(".drvl",dir_path)==False):
+        print("Exiting program, This folder has not been intialized/ .drvl doesnt exist, Use init command to intialize ")
+        exit()
+
+    print_status(dir_path)
     
 else:
     print("Invalid CLA, Exiting program, Kindly recompile")
